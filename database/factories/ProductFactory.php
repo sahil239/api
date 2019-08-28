84785a00efb00c7d2c004182e83e915984506952
sahildesai <sahil166239>
2019-08-28 17:32:20 +0700

    USE FAKER TO ADD DATA TO DB

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,5 +8,11 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         //
+
+        'name' => $faker-> word,
+        'description' => $faker-> sentence,
+        'price' => $faker->numberBetween(1500,3500),
+        'stock' => $faker->numberBetween(10, 75),
+        'discount' => $faker->numberBetween(5,20)
     ];
 });
